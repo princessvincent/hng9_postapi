e83030deac8f2c48ec3beff5c81adae7d4932669
princessvincent <priscavincent2018@gmail.com>
2022-11-04 16:51:59 -0800

    postapi

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function arithemetic(Request $request)
+    {
+        //  $x = 20;
+        //  $y = 30;
+        // $x;
+        // $y;
+        // $operation_type;
+        // $operation_types = ["addition", "substraction", "multiplication"];
+        // if ($operation_types = null) {
+        //     return response()->json([
+        //         'status' => 'error',
+        //         'message' => "Invalid operation type provided.",
+        //     ]);
+        // }
+        // $result = "";
+        // switch ($operation_types) {
+        //     case "addition":
+        //         $result = $x + $y;
+        //         break;
+        //     case "substraction":
+        //         $result = $x - $y;
+        //         break;
+        //     case "multiplication":
+        //         $result = $x * $y;
+        //         break;
+        //     default:
+        //         break;
+        // }
+
+        // return response()->json([
+        //     'status' => 'success',
+        //     'SlackName' => "PriscaEbuka",
+        //     'operation_types' => $operation_types,
+        //     'Result' => $result,
+        // ]);
+
+        // if (!$result) {
+        //     return response()->json([
+        //         'status' => 'error',
+        //         'message' => "Can't complete this method of request",
+        //     ]);
+        // }
+
+$first_number = $request->input('x');
+$second_number = $request->input('y');
+$operator = $request->input('operation_types');
+$result = '';
+if (is_numeric($first_number) && is_numeric($second_number)) {
+    switch ($operator) {
+        case "add":
+           $result = $first_number + $second_number;
+            break;
+        case "subtract":
+           $result = $first_number - $second_number;
+            break;
+        case "multiply":
+            $result = $first_number * $second_number;
+            break;
+        case "divide":
+            $result = $first_number / $second_number;
+    }
+    return response()->json([
+            'status' => 'success',
+            'slackUsername' => "PriscaEbuka",
+            'operation_types' => $operator,
+            'Result' => $result,
+        ]);
+}
+
+    }
+}
